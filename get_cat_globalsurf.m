9e928505dbf536d0c6c4a459f5278936e5e4e781
parekhpravesh <praveshparekh@gmail.com>
2021-04-22 20:14:40 +0530

    returning global surface measures as a variable

--- get_cat_globalsurf.m
+++ get_cat_globalsurf.m
@@ -1,14 +1,15 @@
-function get_cat_globalsurf(dir_surf, dir_output)
+function globalSurf = get_cat_globalsurf(dir_surf, dir_output, toWrite)
 % Function to compile whole brain surface summary measures from CAT 
 % segmentation
 %% Inputs:
 % dir_surf:         full path to surf directory containing various surface
 %                   data files (such as cortical thickness)
 % dir_output:       full path to where results should be saved
+% toWrite:          true or false indicating if the csv file should be
+%                   written out
 % 
 %% Output:
-% An csv file named 'SurfMeasures_WholeBrain' is written in dir_output that
-% contains the following information:
+% globalSurf is a table type variable that contains the following columns:
 %   * SubjectID
 %   * Mean PBT cortical thickness
 %   * Standard deviation of PBT cortical thickness 
@@ -58,6 +59,9 @@ function get_cat_globalsurf(dir_surf, dir_output)
 %   * Mean Toro's gyrification index [right hemisphere]
 %   * Standard deviation Toro's gyrification index [right hemisphere]
 % 
+% If toWrite is true, a csv file named 'SurfMeasures_WholeBrain' is written 
+% in dir_output that contains the above columns
+% 
 %% Notes:
 % If any of the surface measure files are missing, those measures are
 % skipped from compilation
@@ -75,6 +79,7 @@ function get_cat_globalsurf(dir_surf, dir_output)
 % 
 %% Defaults:
 % dir_output:   same as dir_surf
+% toWrite:      true
 % 
 %% Author(s):
 % Parekh, Pravesh
@@ -100,6 +105,15 @@ else
     end
 end
 
+% Check toWrite
+if ~exist('toWrite', 'var') || isempty(toWrite)
+    toWrite = true;
+else
+    if ~islogical(toWrite)
+        error('toWrite should be either true or false');
+    end
+end
+
 %% Make a list of subjects 
 list_pbt = dir(fullfile(dir_surf, 'lh.pbt.*'));
 if isempty(list_pbt)
@@ -107,93 +121,93 @@ if isempty(list_pbt)
 end
 
 %% Compile measures
-res = cell(length(list_pbt),37);
+globalSurf = cell(length(list_pbt),37);
 
 % Loop over each subject file and summarize values
 for subjs  = 1:length(list_pbt)
         
     % Get subject ID
-    tmp_name     = regexprep(strrep(list_pbt(subjs).name, 'lh.pbt.', ''), {'_T1w_RAS', '_T1w'}, '');
-    res{subjs,1} = tmp_name;
+    tmp_name            = regexprep(strrep(list_pbt(subjs).name, 'lh.pbt.', ''), {'_T1w_RAS', '_T1w'}, '');
+    globalSurf{subjs,1} = tmp_name;
     
     % Get PBT info
-    dat_LH          = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, list_pbt(subjs).name));
-    dat_RH          = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'rh.pbt.')));
-    res{subjs, 2}   = mean([dat_LH; dat_RH]);
-    res{subjs, 3}   = std([dat_LH;  dat_RH]);
-    res{subjs, 14}  = mean(dat_LH);
-    res{subjs, 15}  = std(dat_LH);
-    res{subjs, 16}  = mean(dat_RH);
-    res{subjs, 17}  = std(dat_RH);
+    dat_LH                  = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, list_pbt(subjs).name));
+    dat_RH                  = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'rh.pbt.')));
+    globalSurf{subjs, 2}    = mean([dat_LH; dat_RH]);
+    globalSurf{subjs, 3}    = std([dat_LH;  dat_RH]);
+    globalSurf{subjs, 14}   = mean(dat_LH);
+    globalSurf{subjs, 15}   = std(dat_LH);
+    globalSurf{subjs, 16}   = mean(dat_RH);
+    globalSurf{subjs, 17}   = std(dat_RH);
     
     % Get cortical thickness
     try
-        dat_LH          = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'lh.thickness.')));
-        dat_RH          = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'rh.thickness.')));
-        res{subjs, 4}   = mean([dat_LH; dat_RH]);
-        res{subjs, 5}   = std([dat_LH;  dat_RH]);
-        res{subjs, 18}  = mean(dat_LH);
-        res{subjs, 19}  = std(dat_LH);
-        res{subjs, 20}  = mean(dat_RH);
-        res{subjs, 21}  = std(dat_RH);
+        dat_LH                  = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'lh.thickness.')));
+        dat_RH                  = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'rh.thickness.')));
+        globalSurf{subjs, 4}    = mean([dat_LH; dat_RH]);
+        globalSurf{subjs, 5}    = std([dat_LH;  dat_RH]);
+        globalSurf{subjs, 18}   = mean(dat_LH);
+        globalSurf{subjs, 19}   = std(dat_LH);
+        globalSurf{subjs, 20}   = mean(dat_RH);
+        globalSurf{subjs, 21}   = std(dat_RH);
     catch
         warning(['Cortical thickness file not found for: ', tmp_name]);
     end
     
     % Get gyrification
     try
-        dat_LH          = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'lh.gyrification.')));
-        dat_RH          = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'rh.gyrification.')));
-        res{subjs, 6}   = mean([dat_LH; dat_RH]);
-        res{subjs, 7}   = std([dat_LH;  dat_RH]);
-        res{subjs, 22}  = mean(dat_LH);
-        res{subjs, 23}  = std(dat_LH);
-        res{subjs, 24}  = mean(dat_RH);
-        res{subjs, 25}  = std(dat_RH);
+        dat_LH                  = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'lh.gyrification.')));
+        dat_RH                  = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'rh.gyrification.')));
+        globalSurf{subjs, 6}    = mean([dat_LH; dat_RH]);
+        globalSurf{subjs, 7}    = std([dat_LH;  dat_RH]);
+        globalSurf{subjs, 22}   = mean(dat_LH);
+        globalSurf{subjs, 23}   = std(dat_LH);
+        globalSurf{subjs, 24}   = mean(dat_RH);
+        globalSurf{subjs, 25}   = std(dat_RH);
     catch
         warning(['Gyrification file not found for: ', tmp_name]);
     end
     
     % Get sulcal depth
     try
-        dat_LH          = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'lh.depth.')));
-        dat_RH          = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'rh.depth.')));
-        res{subjs, 8}   = mean([dat_LH; dat_RH]);
-        res{subjs, 9}   = std([dat_LH;  dat_RH]);
-        res{subjs, 26}  = mean(dat_LH);
-        res{subjs, 27}  = std(dat_LH);
-        res{subjs, 28}  = mean(dat_RH);
-        res{subjs, 29}  = std(dat_RH);
+        dat_LH                  = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'lh.depth.')));
+        dat_RH                  = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'rh.depth.')));
+        globalSurf{subjs, 8}    = mean([dat_LH; dat_RH]);
+        globalSurf{subjs, 9}    = std([dat_LH;  dat_RH]);
+        globalSurf{subjs, 26}   = mean(dat_LH);
+        globalSurf{subjs, 27}   = std(dat_LH);
+        globalSurf{subjs, 28}   = mean(dat_RH);
+        globalSurf{subjs, 29}   = std(dat_RH);
     catch
         warning(['Depth file not found for: ', tmp_name]);
     end
     
     % Get fractal dimension
     try
-        dat_LH          = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'lh.fractaldimension.')));
-        dat_RH          = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'rh.fractaldimension.')));
-        res{subjs, 10}  = mean([dat_LH; dat_RH]);
-        res{subjs, 11}  = std([dat_LH;  dat_RH]);
-        res{subjs, 30}  = mean(dat_LH);
-        res{subjs, 31}  = std(dat_LH);
-        res{subjs, 32}  = mean(dat_RH);
-        res{subjs, 33}  = std(dat_RH);
+        dat_LH                  = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'lh.fractaldimension.')));
+        dat_RH                  = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, strrep(list_pbt(subjs).name, 'lh.pbt.', 'rh.fractaldimension.')));
+        globalSurf{subjs, 10}   = mean([dat_LH; dat_RH]);
+        globalSurf{subjs, 11}   = std([dat_LH;  dat_RH]);
+        globalSurf{subjs, 30}   = mean(dat_LH);
+        globalSurf{subjs, 31}   = std(dat_LH);
+        globalSurf{subjs, 32}   = mean(dat_RH);
+        globalSurf{subjs, 33}   = std(dat_RH);
     catch
         warning(['Depth file not found for: ', tmp_name]);
     end    
     
     % Get Toro's gyrification index
     try
-        tmp_LH          = dir(fullfile(dir_surf, ['lh.toroGI*.', tmp_name, '*']));
-        tmp_RH          = dir(fullfile(dir_surf, ['rh.toroGI*.', tmp_name, '*']));
-        dat_LH          = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, tmp_LH(1).name));
-        dat_RH          = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, tmp_RH(1).name));
-        res{subjs, 12}  = mean([dat_LH; dat_RH]);
-        res{subjs, 13}  = std([dat_LH;  dat_RH]);
-        res{subjs, 34}  = mean(dat_LH);
-        res{subjs, 35}  = std(dat_LH);
-        res{subjs, 36}  = mean(dat_RH);
-        res{subjs, 37}  = std(dat_RH);
+        tmp_LH                  = dir(fullfile(dir_surf, ['lh.toroGI*.', tmp_name, '*']));
+        tmp_RH                  = dir(fullfile(dir_surf, ['rh.toroGI*.', tmp_name, '*']));
+        dat_LH                  = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, tmp_LH(1).name));
+        dat_RH                  = cat_io_FreeSurfer('read_surf_data', fullfile(dir_surf, tmp_RH(1).name));
+        globalSurf{subjs, 12}   = mean([dat_LH; dat_RH]);
+        globalSurf{subjs, 13}   = std([dat_LH;  dat_RH]);
+        globalSurf{subjs, 34}   = mean(dat_LH);
+        globalSurf{subjs, 35}   = std(dat_LH);
+        globalSurf{subjs, 36}   = mean(dat_RH);
+        globalSurf{subjs, 37}   = std(dat_RH);
     catch
         warning(['Toro GI files not found for: ', tmp_name]);
     end        
@@ -218,8 +232,10 @@ var_names = {'SubjectID';                                               ...
              'Mean_LH_FractalDimension';    'SD_LH_FractalDimension';   ...
              'Mean_RH_FractalDimension';    'SD_RH_FractalDimension';   ...
              'Mean_LH_ToroGI';              'SD_LH_ToroGI';             ...
-             'Mean_RH_ToroGI';              'SD_RH_ToroGI'};
-             
-res = cell2table(res, 'VariableNames', var_names);
-writetable(res, fullfile(dir_output, 'SurfMeasures_WholeBrain.csv'));
+             'Mean_RH_ToroGI';              'SD_RH_ToroGI'};          
+globalSurf = cell2table(globalSurf, 'VariableNames', var_names);
+
+% Write table, if required
+if toWrite
+    writetable(globalSurf, fullfile(dir_output, 'SurfMeasures_WholeBrain.csv'));
 end
